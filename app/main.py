05c84a933e7ce600dec24f7d493fceb66a946e22
vijay <vp.gd.msw@gmail.com>
2022-08-04 17:05:36 +0530

    anothre change in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 # from . import models
-from .database import engine
+# from .database import engine
 from .routers import user, post, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
  

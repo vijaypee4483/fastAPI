8074301b6b82201d0a2d85556a8a88060a3eb0f8
vijay <vp.gd.msw@gmail.com>
2022-08-10 17:11:35 +0530

    added test for post

--- tests/conftest.py
+++ tests/conftest.py
@@ -54,6 +54,16 @@ def test_user(client):
     return new_user
 
 @pytest.fixture
+def test_user2(client):
+    user_data = {"email": "abc@gmail.com", "password": "12345"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    print(res.json())
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+@pytest.fixture
 def token(test_user):
     return create_access_token({'user_id': test_user['id']})
 
@@ -67,11 +77,12 @@ def authorized_client(client, token):
 
 
 @pytest.fixture
-def test_posts(test_user, session):
+def test_posts(test_user, test_user2, session):
     posts_data = [
         {"title": "1st title", "content": "1st content", "owner_id": test_user['id']},
         {"title": "2nd title", "content": "2nd content", "owner_id": test_user['id']},
         {"title": "3rd title", "content": "3rd content", "owner_id": test_user['id']},
+        {"title": "4th title", "content": "4th content", "owner_id": test_user2['id']}
     ]
 
     def create_posts_model(post):
@@ -81,6 +92,11 @@ def test_posts(test_user, session):
     posts = list(post_map)
 
     session.add_all(posts)
+    # session.add_all([
+    #     models.Post({"title": "1st title", "content": "1st content", "owner_id": test_user['id']}),
+    #     models.Post({"title": "2nd title", "content": "2nd content", "owner_id": test_user['id']}),
+    #     models.Post({"title": "3rd title", "content": "3rd content", "owner_id": test_user['id']})
+    #     ])
     session.commit()
     session.query(models.Post).all()
 
